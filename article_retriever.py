3fced8e7040fa3456e0ec2423b71afddd77017ae
Aashish Mehta <aashish_mehta@outlook.com>
2023-03-15 00:49:14 +0000

    Add notebook and retrieval methods

---
+++ article_retriever.py
@@ -0,0 +1,57 @@
+import re
+from urllib.parse import urlparse
+
+from soup_methods import *
+
+bbc_url = 'https://www.bbc.co.uk'
+pink_url = 'https://www.thepinknews.com'
+_filter_dict = {
+    bbc_url: {'data-component': 'text-block'},
+    pink_url: {'class': 'article__content'}
+}
+
+def get_articles_for_topic(topic_url):
+    base_url = _get_base_url(topic_url)
+    if base_url == bbc_url:
+        return _get_articles_for_bbc_topic(topic_url)
+    elif base_url == pink_url:
+        return _get_articles_for_pink_topic(topic_url)
+    else:
+        print(topic_url + ' is not a supported url')
+
+def get_info_from_article(article_url):
+    filter = _get_filter(article_url)
+    soup = get_soup_from_url(article_url)
+    heading = get_header_from_soup(soup)
+    text = get_div_text_from_soup(soup, filter)
+    return [heading, text]
+
+def _get_articles_for_bbc_topic(topic_url):
+    prefix = _get_base_url(topic_url)
+    soup = get_soup_from_url(topic_url)   
+    li_tags = soup.find_all('li')
+    url_list = []
+    for li_tag in li_tags:
+        article_div = li_tag.find('div', {'type': 'article'})
+        if article_div:
+            url_list.append(prefix + get_href_from_element(article_div))
+    return url_list
+
+def _get_articles_for_pink_topic(topic_url):
+    soup = get_soup_from_url(topic_url)
+    article_divs = soup.find_all('div', class_=re.compile('article-.*__container'))
+    url_list = []
+    for article_div in article_divs:
+        url_list.append(get_href_from_element(article_div))
+    return url_list
+
+def _get_filter(url):
+    base_url = _get_base_url(url)
+    if base_url in _filter_dict:
+        return _filter_dict[base_url]
+    else:
+        print(url + ' is not a supported url')
+
+def _get_base_url(url):
+    parsed_uri = urlparse(url)
+    return '{uri.scheme}://{uri.netloc}'.format(uri=parsed_uri)
---
+++ soup_methods.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+def get_soup_from_url(page_url):
+    response = requests.get(page_url)
+    return BeautifulSoup(response.text, features="html.parser")
+
+def get_div_text_from_soup(soup, filter):
+    text_blocks = soup.find_all('div', filter)
+    text_list = [text_block.get_text(strip=True) for text_block in text_blocks]
+    return ' '.join(text_list)
+
+def get_header_from_soup(soup):
+    header = soup.find('h1')
+    return header.text
+
+def get_href_from_element(element):
+    return element.find('a')['href']
