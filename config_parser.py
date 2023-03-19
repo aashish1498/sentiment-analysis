f1983ef260078805959473524d465b4fcf0e9ea5
Aashish Mehta <aashish_mehta@outlook.com>
2023-03-19 01:07:07 +0000

    fix: return config data

--- config_parser.py
+++ config_parser.py
@@ -3,5 +3,6 @@ import configparser
 config = configparser.ConfigParser()
 config.read('config.ini')
 
+
 def retrieve_topic_url(source, topic_name):
-    topic_url = config.get(source, topic_name)
+    return config.get(source, topic_name)
